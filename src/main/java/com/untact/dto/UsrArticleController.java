06978e6f0bd7b53eef0dfd7af85cd586ab3841eb
AnChangHyune <ymy6534@gmail.com>
2021-03-25 19:03:30 +0900

    게시물에 수정날짜 칼람 추가

--- src/main/java/com/untact/dto/UsrArticleController.java
+++ src/main/java/com/untact/dto/UsrArticleController.java
@@ -20,8 +20,8 @@ public class UsrArticleController {
 	public UsrArticleController() {
 		articles = new ArrayList<>();
 
-		articles.add(new Article(++articlesLastId, "2021-12-25 13:05:12", "제목1", "내용1"));
-		articles.add(new Article(++articlesLastId, "2021-12-25 13:05:12", "제목2", "내용2"));
+		articles.add(new Article(++articlesLastId, Utill.getNowDateStr(), Utill.getNowDateStr(),"제목1", "내용1"));
+		articles.add(new Article(++articlesLastId, Utill.getNowDateStr(), Utill.getNowDateStr(),"제목2", "내용2"));
 	}
 
 	@RequestMapping("/usr/article/detail")
@@ -40,8 +40,8 @@ public class UsrArticleController {
 	@ResponseBody
 	public Map<String, Object> doAdd(String title, String body) {
 		String regDate = Utill.getNowDateStr();
-		
-		articles.add(new Article(++articlesLastId, regDate, title, body));
+		String updateDate = regDate;
+		articles.add(new Article(++articlesLastId, regDate, updateDate,title, body));
 
 		Map<String, Object> rs = new HashMap<>();
 		rs.put("resultCode", "S-1");
@@ -98,7 +98,7 @@ public class UsrArticleController {
 			rs.put("msg", String.format("%d번 게시물은 존재하지 않습니다.", id));
 			return rs;
 		}
-		
+		selArticle.setUpdateDate(Utill.getNowDateStr());
 		selArticle.setTitle(title);
 		selArticle.setBody(body);
 		
